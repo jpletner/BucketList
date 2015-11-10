2a1d24c0f92297f55e8176578a271ea897801935
Jenny Pletner <jennykay55@gmail.com>
2015-11-10 12:57:50 -0800

    Completed capybara testing

--- spec/features/profile_page_spec.rb
+++ spec/features/profile_page_spec.rb
@@ -18,7 +18,6 @@ describe "user profile page" do
    fill_in "Email", :with => "chuck.norris@gmail.com"
    fill_in 'Password', :with => "password"
    click_button "Log in"
-   click_link "View Profile"
    #The following fields should be displayed on the profile page
    expect(page).to have_content "Chuck Norris"
    expect(page).to have_content "city"
@@ -45,16 +44,22 @@ it "user should be able to add adventures and view them" do
    fill_in 'About', :with => "interests"
    fill_in 'Current password', :with => "password"
    click_button "Update"
-   click_link "View Profile"
    expect(page).to have_content "interests"
    fill_in 'Adventure Name', :with => "sky diving"
    fill_in 'Description', :with => "I love sky diving"
-   fill_in 'Due Date', :with => "12/20/2015"
-   fill_in 'Priority', :with => "2"
+   fill_in 'Due Date', :with => "12" "20" "2015"    
+   attach_file('adventure[image]', fixture_image_path)
    click_button "Add"
    expect(page).to have_content "sky diving"
    expect(page).to have_content "I love sky diving"
-
+   expect(page).to have_xpath('//img[contains(@src,"ruby.jpg")]')
 end
      #page is currently routing to the index file on completion. we should re-route to the profile page.
  end
+
+##create path to test image
+def fixture_image_path
+    Rails.root.join('spec', 'support', 'ruby.jpg')
+end
+
+##before(:each) do code in here end
