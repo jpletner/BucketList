d2556c75cd4f26dcdc6dd06ce603b37cfcdd0c49
Jenny Pletner <jennykay55@gmail.com>
2015-11-15 22:51:15 -0800

    added some bootstrap classes to forms, fixed image sizes on show and edit pages

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -3,7 +3,7 @@ class Adventure < ActiveRecord::Base
     
     has_many :pictures, :dependent => :destroy
     
-    has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "400x400" }
+    has_attached_file :image, styles: { small: "64x64", med: "200x200", large: "400x400" }
     
     validates_attachment :image,
         content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
