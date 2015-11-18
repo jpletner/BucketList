eff6253e7912b472f2be3c0dc312061606ab5115
Jenny Pletner <jennykay55@gmail.com>
2015-11-18 14:51:02 -0800

    added new structure to View Profile Page

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,7 @@
 class Picture < ActiveRecord::Base
     belongs_to :adventure
 
-    has_attached_file :photo, styles: { small: "64x64", med: "100x100", large: "400x400" }
+    has_attached_file :photo, styles: { small: "64x64", med: "200x200", large: "400x400" }
 
     validates_attachment :photo,
             content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] },
