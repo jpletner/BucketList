b89d751bd54f3d8d5e156fe37e425f2b6a3e84fe
Jenny Pletner <jennykay55@gmail.com>
2015-11-25 09:20:36 -0800

    Adding S3 credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['learnbucketlist'],
+    :access_key_id => ENV['AKIAIKDQVLXED76F32VA'],
+    :secret_access_key => ENV['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']
+  }
+}
 end
